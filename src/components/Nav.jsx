820c79c7e1e4d50efd38f28c2ae690ea433aaa81
ilyes-shn <infinityilyes@gmail.com>
2020-09-02 22:04:19 +0100

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>
-            <ul class="nav justify-content-center bg-pimary">
-                <li class="nav-item">
-                    <a class="nav-link " href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="#">contact us</a>
-                </li>
-            </ul>
-        </div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    
+                </ul>
+                
+            </div>
+        </nav>
     )
 }
 

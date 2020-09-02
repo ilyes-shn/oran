341d48be07b3186028a19080a7a3a85482d6e1d6
ilyes-shn <infinityilyes@gmail.com>
2020-09-02 21:57:36 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <h1>Hello</h1>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link " href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#">contact us</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav

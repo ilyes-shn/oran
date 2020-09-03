f02515f027a653a2bc66ad0d7a1d7a44de0b30a2
ilyes-shn <infinityilyes@gmail.com>
2020-09-03 12:13:04 +0100

    edits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About' 
 
 function App() {
   return (
     <Layout>
     <div class="container">
-      <h1>Hello</h1>
+      <Router>
+        <Switch>
+          <Route to='/'>
+            <Home />
+          </Route>    
+          <Route to='/about'>
+            <About/>
+          </Route>      
+        </Switch>
+      </Router>
     </div>
     </Layout>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-            <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link className="navbar-brand" href="/">Wahren</Link>
             <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                 aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -11,10 +12,10 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="collapsibleNavId">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <Link className="nav-link" href="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <Link className="nav-link" href="/about">About</Link>
                     </li>
                     
                 </ul>

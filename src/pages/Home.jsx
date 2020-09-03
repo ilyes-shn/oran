1c2c9a1b059f2ee97e2f124d10044fc06075cfd4
ilyes-shn <infinityilyes@gmail.com>
2020-09-03 14:00:22 +0100

    jumbo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <div className="jumbotron">
+                <h1 className="display-3">El Bahia</h1>
+                <p className="lead">React App</p>
+                <hr className="my-2" />
+                <p>More info</p>
+            </div>
+        </>
     )
 }
 

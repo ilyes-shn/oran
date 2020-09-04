8e4b330668a51d9f53000ac76cdf6c3d2e73cacd
ilyes-shn <infinityilyes@gmail.com>
2020-09-04 21:28:08 +0100

    ico

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const About = () => {
     return (
-        <div>
-            <h1>I'm Elyes Web Developer</h1>
-            <h5>I'm specialized in Javascript and React</h5>
-            <hr/>
-            <p>I'll be really happy to build amazing projects for you</p>
+        
+        <div >
+            <motion.h1 initial={{ x: -200 }} animate={{ x: 0 }}>I'm Elyes Web Developer</motion.h1>
+            <motion.h5 initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ delay: 0.2}}>I'm specialized in Javascript and React</motion.h5>
+            <motion.hr initial={{ x: 200 }} animate={{ x: 0}} />
+            <motion.p initial={{ y: 200 }} animate={{ y: 0}} transition={{ ease: 'easeOut'}} >I'll be really happy to build amazing projects for you</motion.p>
+            <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ delay: 0.2}} style={{ position: "absolute", bottom: 0}}>infinityilyes@gmail.com</motion.p>
         </div>
+        
     )
 }
 

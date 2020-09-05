5a5962ab9eb06e6068e134f6fc1bb4202e95821c
ilyes-shn <infinityilyes@gmail.com>
2020-09-05 11:21:00 +0100

    code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
         <motion.div>
             <motion.h1 initial={{ y: -50, opacity: 0}} animate={{ y: 0, opacity: 1}} whileHover={{ scale: 1.02}}>This is React with Framer Motion</motion.h1>
             <motion.hr initial={{ opacity: 0}} animate={{ opacity: 1}}/>
+<code>Under Construction</code>
         </motion.div>
     )
 }

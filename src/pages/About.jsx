d70698c894ecdacceee58d68dfb69984c29ed0c3
ilyes-shn <infinityilyes@gmail.com>
2020-09-05 17:40:28 +0100

    scroll

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 const About = () => {
     return (
         
-        <div>
+        <div style={{ height: '100vh' }}>
             <motion.h1 initial={{ x: -200 }} animate={{ x: 0 }}>I'm Elyes Web Developer</motion.h1>
             <motion.h5 initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ delay: 0.2}}>I'm specialized in Javascript and React</motion.h5>
             <motion.hr initial={{ x: 200 }} animate={{ x: 0}} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+
 const Home = () => {
     return (
-        <motion.div>
+        <motion.div style={{ height: '100vh' }}>
             <motion.h1 initial={{ y: -50, opacity: 0}} animate={{ y: 0, opacity: 1}} whileHover={{ scale: 1.02}}>This is React with Framer Motion</motion.h1>
             <motion.hr initial={{ opacity: 0}} animate={{ opacity: 1}}/>
-<code>Under Construction</code>
+            <code>Under Construction</code>
+          
         </motion.div>
     )
 }
 
 export default Home
+
+
+

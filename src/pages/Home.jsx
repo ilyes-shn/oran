766bd13a55b3db81595787d87ab599ae31651dec
ilyes-shn <infinityilyes@gmail.com>
2020-09-10 17:50:11 +0100

    fade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import { motion } from 'framer-motion'
 const Home = () => {
 
     return (
-        <div className="Home">
+        <motion.div className="Home">
             <motion.h1
             initial={{ y: -50, opacity: 0}}
             animate={{ y: 0, opacity: 1}}
             whileHover={{ scale: 1.02}}>This is React with Framer Motion</motion.h1>
             <motion.hr initial={{ opacity: 0}} animate={{ opacity: 1}}/>
             <br />
-            <div className="row">
+            <motion.div className="row" initial={{ opacity: 0, y: '50vh'}} animate={{opacity: 1, y: 0}} transition={{ease: 'easeOut', duration: 0.2}}>
               <div className="col-md-4">
                 <h3>React Js</h3>
                 <p>Is a Javascript framework for creating SPA and PWA</p>
@@ -25,9 +25,8 @@ const Home = () => {
                 <h3>Bootstrap</h3>
                 <p>Is a CSS framework created by Twitter</p>
               </div>
-            </div>
-
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
